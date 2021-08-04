e41f4aa9bc8cd50112efde61964395f97d4da4db
root <root@LAPTOP-33U5DJ2G.localdomain>
2021-08-05 01:12:29 +0800

    Updates

---
+++ precache-manifest.b330042d15f6a6a2915595aae076383d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179f34a42eb2704db8354e10655b0dc5",
+    "url": "/zombie-game21/index.html"
+  },
+  {
+    "revision": "3d659e3f72f3f77ae7b9",
+    "url": "/zombie-game21/static/css/main.f6c31f70.chunk.css"
+  },
+  {
+    "revision": "a1c045577786aae09aef",
+    "url": "/zombie-game21/static/js/2.e29d2a93.chunk.js"
+  },
+  {
+    "revision": "492ef63e9fe32957cbfc69d250bfb711",
+    "url": "/zombie-game21/static/js/2.e29d2a93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d659e3f72f3f77ae7b9",
+    "url": "/zombie-game21/static/js/main.03774008.chunk.js"
+  },
+  {
+    "revision": "a7dec51324169aa39fbf",
+    "url": "/zombie-game21/static/js/runtime-main.43225e55.js"
+  },
+  {
+    "revision": "d2b2559b651e6a50f8d43f206dcdb8e7",
+    "url": "/zombie-game21/static/media/Btn_Register_Normal.d2b2559b.png"
+  },
+  {
+    "revision": "d3684ace4f10c62c9209b45d3bb724c2",
+    "url": "/zombie-game21/static/media/Btn_Register_Over.d3684ace.png"
+  },
+  {
+    "revision": "64a6f16c4f5ef7e189e75dfc0a52a25f",
+    "url": "/zombie-game21/static/media/homecard@2x.64a6f16c.png"
+  },
+  {
+    "revision": "9d0437bbb5e705bad95ac929361faf3e",
+    "url": "/zombie-game21/static/media/index_bg.9d0437bb.png"
+  },
+  {
+    "revision": "41236031fc933b053717ef1760601e36",
+    "url": "/zombie-game21/static/media/start_btn.41236031.png"
+  },
+  {
+    "revision": "2a4a2235f6ed0e838b49a3f1bec4aae1",
+    "url": "/zombie-game21/static/media/tester-bg@2x.2a4a2235.png"
+  },
+  {
+    "revision": "b102b8003aab95865cb7363428273a16",
+    "url": "/zombie-game21/static/media/walls.b102b800.jpg"
+  },
+  {
+    "revision": "08087132d6dcfcd5c5f471880c104712",
+    "url": "/zombie-game21/static/media/zombierun.08087132.png"
+  }
+]);
